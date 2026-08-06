04b9236335c48f251b44498d73438fc0b37f1e00
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2026-08-06 19:19:24 +0530

    Enhance HTML generation in markdown file processing and add directory creation for output files

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,45 @@ fn main() {
     println!("Found files: {:?}", md_files);
 
     let md_files = scanner::get_markdown_files(input_dir);
-
+    
     for file in md_files {
         let page = parser::parse_file(&file);
-        println!("Title: {}", page.frontmatter.title);
-        println!("HTML:\n{}", page.generated);    
+        
+        let html_document = format!(
+            r#"<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{}</title>
+</head>
+<body>
+    <header>
+        <h1>My Rust SSG</h1>
+    </header>
+    <main>
+        {}
+    </main>
+</body>
+</html>"#, 
+            page.frontmatter.title, 
+            page.generated         
+        );
+
+        println!("Generated HTML for: {}", page.frontmatter.title);
+
+        let output_path = file
+            .replacen(input_dir, output_dir, 1)
+            .replace(".md", ".html");
+
+        if let Some(parent_dir) = std::path::Path::new(&output_path).parent() {
+            std::fs::create_dir_all(parent_dir).expect("Failed to create input directories");
+        }
+
+        std::fs::write(&output_path, html_document).expect("Failed to write HTML file");
+        
+        println!("Successfully built: {}", output_path)
+
     }
     
 }
